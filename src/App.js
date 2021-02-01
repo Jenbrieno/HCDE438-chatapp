0bd4ce6a686a593bb018c465e167f0e1aef7e966
Yvonne Huang <huangq9@uw.edu>
2021-01-31 18:33:49 -0800

    style

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
-import logo from './logo.svg';
+
+import { useState } from "react"
 import './App.css';
+import Textinput from './Textinput';
+
 
 function App() {
-  return (
-    <div className="App">
+  const [messages, setMessages] = useState([{text:'hello'}])
+  return <div className="App">
+
       <header className = "header">
         <div className = "logo" />
-        CHAT
+        LetsChat
       </header>
+
+      {/* <div className="message">
+        {messages[0].text}
+      </div> 
+  */}
+
+    <div className = "text">
+
+      <div className = "text1">
+        hello this is a message 
+      </div>
+
+      <div className = "text1">
+        lol
+      </div>
+
+      <div className = "text1">
+        bye 
+      </div>
+
+  
     </div>
-  );
+
+      <Textinput 
+        send={(t)=> setMessages([{text:t}])}
+      />
+
+    </div>
+
 }
 
 export default App;
---
+++ src/Textinput.js
@@ -0,0 +1,34 @@
+import { useState } from "react"
+
+
+function Textinput(props) {
+    const[text, setText] = useState('')
+
+    function sendMessage(){
+        props.send(text)
+        setText('')
+    }
+
+    function KeyPress(e){
+        if(e.key==='Enter'){
+            sendMessage()
+        }
+    }
+
+    return <footer className = "text-input">
+        <input className = "input"
+            placeholder = "   start typing!"
+            value = {text}
+            onChange = {e=> setText(e.target.value)}
+            onKeyPress= {KeyPress}
+        />
+  
+        <button className = "button"
+            onClick={sendMessage}>
+        </button>
+      
+
+    </footer>
+}
+
+export default Textinput
